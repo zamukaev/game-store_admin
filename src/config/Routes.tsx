b334331e460e38cef8fdcb6442b6fddf935ed10b
zamukaev <abu.safiia2016@mail.com>
2024-08-15 15:48:13 +0200

    add alias

--- src/config/Routes.tsx
+++ src/config/Routes.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../pages/home/Home";
-import Layout from "../components/layout/Layout";
-import Products from "../pages/products/Products";
-import Product from "../pages/product/Product";
-import Hits from "../pages/hits/Hits";
-import Promotion from "../pages/promotion/Promotion";
-import Categories from "../pages/categories/Categories";
-import Category from "../pages/category/Category";
+
+import Home from "@/pages/home/Home";
+import Layout from "@/components/layout/Layout";
+import Products from "@/pages/products/Products";
+import Product from "@/pages/product/Product";
+import Hits from "@/pages/hits/Hits";
+import Promotion from "@/pages/promotion/Promotion";
+import Categories from "@/pages/categories/Categories";
+import Category from "@/pages/category/Category";
 
 export const PrivateRoutes: FC = () => {
     return (
